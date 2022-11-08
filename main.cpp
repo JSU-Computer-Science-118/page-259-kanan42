6dc3664f2976200b411bcfab09a8c69458117552
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-07 23:18:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-//Hannah Coleman J00963674 
-
 #include<iostream>
 #include<cmath>
 using namespace std;
@@ -10,36 +8,33 @@ int main()
 	double c;
 	double d;
 	double discriminant;
-	double root;
-	double root 1;
-	double root 2;
 	cout << "Enter the value of a :" << endl;
 	cin >> a;
 	cout << "Enter the value of b:" << endl;
 	cin >> b;
 	cout << "Enter the value of c:" << endl;
 	cin >> c;
-	
-	d = pow(b, 2) - (4 * a * c);
-	discriminant = pow(d, 0.5);                                                      
+	d = pow(b, 2) - 4 * a * c;
+	discriminant = pow(d, 0.5);
+                                                                                              
 
 	if (d < 0) {
-		cout << " two complex roots" << endl;
+		cout << "The equation has two complex roots." << endl;
 
 	}
 
 		
 	else if (d == 0) {
-		 root = (-b + discriminant) / (2 * a);
-		cout << "Single root" <<" root: "<< root;
+		double root = (-b + discriminant) / (2 * a);
+		cout << "The equation has a single repeated root." <<" root: "<< root;
 	}
 	
 	 
 	else if (d > 0) {
-		 root1 = (-b + discriminant) /(2*a)
-		root2 = (-b - discriminant) / (2 * a);
-		cout << "Two real roots";
-		cout <<  " root 1 :"
+		double root1 = (-b + discriminant) / (2 * a);
+		double root2 = (-b - discriminant) / (2 * a);
+		cout << "The equation has different roots .";
+		cout << "The roots are:" << " root 1 :"
 			<< root1 << ", " << "root 2 :" << root2 << endl;
 
 	}
