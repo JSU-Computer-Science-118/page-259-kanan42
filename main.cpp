b7d13176eff14a90fd77376a259a6a8dafea040d
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-08 09:00:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,38 @@
-//Hannah Coleman J00963674 
-#include<iostream>
-#include<cmath>
+
+
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-	double a;
-	double b;
-	double c;
-	double d;
-	double discriminant;
-	double root1;
-	double root2;
-	cout<<"Enter the value of a,b,c"<<endl;
-	cin >> a >> b >> c;
-	
-	discriminant = pow(b, 2) - 4 * a * c;
-	root1 = (-b + sqrt(discriminant)) / (2 * a);
-	root2 = (-b + sqrt(discriminant)) / (2 * a);
-	if (discriminant==0) {
-		cout << "The single root is " <<  root1 << endl;
-
-	}
-	else if (discriminant > 0) {
-		cout << "Two real roots\n";
-		cout << "-First root " << root1 <<"\n";
-		cout << "-Second root " << root2 <<"\n";
-
-	}
-	else if (discriminant > 0) {
-		cout << "Complex root\n";
-	}
-	return 0;
-
-
-	
-
-
-
-
+    double a,b,c;
+    int root1;
+    int root2;
+    double discriminant;
+    
+    cout << "Enter the value of a,b,c: ";
+    cin >> a>>b>>c;
+    
+    discriminant = pow(b,2) - (4 * a * c);
+    root1 = (-b + sqrt(discriminant))/(2 * a);
+    root2 = (-b - sqrt(discriminant))/(2 * a);
+    
+    if (discriminant == 0){
+        cout << "The single root is "<<root1<<endl;
+    }
+    
+    else if(discriminant > 0){
+        cout << "Two real roots\n";
+        cout <<"-First root  "  << root1 << "\n";
+        cout <<"-Second root  " << root2 << "\n";
+    }
+    
+    else if (discriminant < 0){
+        cout << "Complex roots\n" << root1 << endl;
+    }
+    
+    return 0;
+    
 }
