3333790103c4e0ab0a21ede169a2558640905cde
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-07 22:59:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,9 @@ int main()
 	double c;
 	double d;
 	double discriminant;
-	cout << "Enter the value of a :" << endl;
-	cin >> a;
-	cout << "Enter the value of b:" << endl;
-	cin >> b;
-	cout << "Enter the value of c:" << endl;
-	cin >> c;
-	d = pow(b, 2) - (4 * a * c);
-	discriminant = pow(d, 0.5);
-                                                                                            
+	cin>> a >> b >> c 
+	d = pow(b, 2) - 4 * a * c;
+	discriminant = pow(d, 0.5);                                                      
 
 	if (d < 0) {
 		cout << "The equation has two complex roots." << endl;
