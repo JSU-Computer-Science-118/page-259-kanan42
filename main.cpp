8da3ca25c38f09177a519816e11b6c5291d58559
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-07 21:30:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,30 +9,33 @@ int main()
 	double c;
 	double d;
 	double discriminant;
-	cout << "Enter the value of a,b, c :" << endl;
-	cin >> a <<b <<c;
+	cout << "Enter the value of a :" << endl;
+	cin >> a;
+	cout << "Enter the value of b:" << endl;
+	cin >> b;
+	cout << "Enter the value of c:" << endl;
+	cin >> c;
 	d = pow(b, 2) - 4 * a * c;
 	discriminant = pow(d, 0.5);
                                                                                               
 
 	if (d < 0) {
-		cout << "The equation has two complex roots." << endl;
+		cout << "Complex root" << endl;
 
 	}
 
 		
 	else if (d == 0) {
 		double root = (-b + discriminant) / (2 * a);
-		cout << "The equation has a single repeated root." <<" root: "<< root;
+		cout << "The single root is "<< root;
 	}
 	
 	 
 	else if (d > 0) {
 		double root1 = (-b + discriminant) / (2 * a);
 		double root2 = (-b + discriminant) / (2 * a);
-		cout << "The equation has different roots .";
-		cout << "The roots are:" << " root 1 :"
-			<< root1 << ", " << "root 2 :" << root2 << endl;
+		cout << "-First root" << root1;
+		cout << "-Second root" << root2;
 
 	}
 	
