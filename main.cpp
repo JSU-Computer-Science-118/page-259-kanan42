0aa9d26d755951cd0e8435241830835e9ca46842
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-08 14:33:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <cmath>
 
@@ -7,35 +5,34 @@ using namespace std;
 
 int main()
 {
-    double a,b,c;
+    double a, b, c;
     double root1;
     double root2;
     double discriminant;
-   
-    cin >> a>>b>>c;
-    
-    discriminant = pow(b,2) - (4 * a * c);
-    root1 = (-b + sqrt(discriminant))/(2 * a);
-    root2 = (-b - sqrt(discriminant))/(2 * a);
-    if (a=0)
-    
-    if (discriminant == 0){
-        cout << "The single root is "<< root1<<endl;
+
+    cin >> a >> b >> c;
+
+    discriminant = pow(b, 2) - (4 * a * c);
+    root1 = (-b + sqrt(discriminant)) / (2 * a);
+    root2 = (-b - sqrt(discriminant)) / (2 * a);
+
+    if (discriminant == 0) {
+        cout << "The single root is " << root1 << endl;
     }
-    
-    else if (a==0){
-        cout<<"Invalid entry";
+
+    else if (a == 0) {
+        cout << "Invalid entry";
     }
-    else if(discriminant > 0){
+    else if (discriminant > 0) {
         cout << "Two real roots\n";
-        cout <<"- First root " << root1 << "\n";
-        cout <<"- Second root "<< root2;
+        cout << "- First root " << root1 << "\n";
+        cout << "- Second root " << root2;
     }
-    
-    else if (discriminant < 0){
+
+    else if (discriminant < 0) {
         cout << "Complex roots";
     }
-    
+
     return 0;
-    
+
 }
